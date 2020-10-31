33b03ff0384c36826234900667804b6bfff6e054
Marcelo Galigniana <marcelogaligniana@gmail.com>
2020-10-31 00:51:49 -0300

    join_with is blank

--- apps/tables/models.py
+++ apps/tables/models.py
@@ -5,7 +5,7 @@ class Table(models.Model):
     x = models.IntegerField()
     y = models.IntegerField()
     orders = models.ManyToManyField("products.Product", through="orders.Order", null=True)
-    join_with = models.ManyToManyField("self", null=True)
+    join_with = models.ManyToManyField("self", null=True, blank=True)
 
     def __str__(self):
         return f"Mesa {self.id} en [{self.x}, {self.y}]"
