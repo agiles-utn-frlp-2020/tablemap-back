a6b5fa8ce1815c97a2b42e99e8d134ed601468f0
Marcelo Galigniana <marcelogaligniana@gmail.com>
2020-11-02 20:44:57 -0300

    M2M null has no effect

--- apps/tables/models.py
+++ apps/tables/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Table(models.Model):
     x = models.IntegerField()
     y = models.IntegerField()
-    orders = models.ManyToManyField("products.Product", through="orders.Order", null=True)
-    join_with = models.ManyToManyField("self", null=True, blank=True)
+    orders = models.ManyToManyField("products.Product", through="orders.Order")
+    join_with = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
         return f"Mesa {self.id} en [{self.x}, {self.y}]"
