167120a6be3556c041f592d18f3c7bafc08cc2f7
Patricio Cistola <patriciocistola@gmail.com>
2020-11-11 20:52:34 -0300

    agregando nombre a la mesa

--- apps/tables/models.py
+++ apps/tables/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Table(models.Model):
+    name = models.CharField(max_length=200)
     x = models.IntegerField()
     y = models.IntegerField()
     orders = models.ManyToManyField("products.Product", through="orders.Order")
-    join_with = models.OneToOneField("self", on_delete=models.CASCADE, null=True, blank=True)
+    join_with = models.OneToOneField(
+        "self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f"Mesa {self.id} en [{self.x}, {self.y}]"
+        return f"Mesa {self.name} con id {self.id} en [{self.x}, {self.y}]"
