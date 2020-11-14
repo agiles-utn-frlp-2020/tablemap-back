f24cfbd0a002713c7b8b8c85bd2ce9002e413c9c
Marcelo Galigniana <marcelogaligniana@gmail.com>
2020-11-14 11:13:56 -0300

    Add default table name at models.py

--- apps/tables/models.py
+++ apps/tables/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Table(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default="Nueva mesa")
     x = models.IntegerField()
     y = models.IntegerField()
     orders = models.ManyToManyField("products.Product", through="orders.Order")
